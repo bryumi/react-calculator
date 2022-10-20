77e6a6bfc47f7ed6817a6d0d785569f6c1bd8b0d
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2022-10-19 22:37:50 -0300

    add botões e display

--- calculadora/src/main.jsx
+++ calculadora/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Calculator from './main/Calculator'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Calculator</h1>
+    <Calculator/>
+
+  </div>
 )
